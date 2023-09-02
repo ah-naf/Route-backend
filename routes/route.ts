7b7391ef1e3953236d9af70bd378924644f75334
ah-naf <sheikhahnafshifat@gmail.com>
2023-09-03 00:04:14 +0600

    Add: add route, edit route, delete route option in backend

--- routes/route.ts
+++ routes/route.ts
@@ -9,7 +9,7 @@ const router = Router();
 router.post("/", jwtVerify, async (req, res) => {
   try {
     const { id, title, flow, published, userId } = req.body;
-    console.log(title)
+    console.log(title);
     if (userId !== req.user)
       return res.status(403).json({ msg: "You are not authorized" });
     if (!id || !title || !flow || !userId)
@@ -39,6 +39,60 @@ router.post("/", jwtVerify, async (req, res) => {
   } catch (error) {
     let msg = "An unknown error occured.";
     if (error instanceof Error) msg = error.message;
+    return res.status(500).json({ msg });
+  }
+});
+
+// Get all Route
+router.get("/", jwtVerify, async (req, res) => {
+  try {
+    const routes = await prisma.route.findMany({
+      where: {
+        userId: req.user,
+      },
+    });
+    return res.status(200).json({ routes });
+  } catch (error) {
+    let msg = "An unknown error occured.";
+    if (error instanceof Error) msg = error.message;
+    return res.status(500).json({ msg });
+  }
+});
+
+// Delete Single Route
+router.delete("/:id", jwtVerify, async (req, res) => {
+  try {
+    await prisma.route.delete({
+      where: {
+        id: req.params.id,
+      },
+    });
+    return res.status(200).json({ msg: "Route deleted successfully" });
+  } catch (error) {
+    let msg = "An unknown error occured.";
+    if (error instanceof Error) msg = error.message;
+    console.log(msg);
+    return res.status(500).json({ msg });
+  }
+});
+
+// Get Single Route
+router.get("/:id", async (req, res) => {
+  try {
+    const route = await prisma.route.findUnique({
+      where: {
+        id: req.params.id,
+      },
+      include: {
+        comments: true,
+        likes: true
+      }
+    });
+    return res.status(200).json({ route });
+  } catch (error) {
+    let msg = "An unknown error occured.";
+    if (error instanceof Error) msg = error.message;
+    console.log(msg);
     return res.status(500).json({ msg });
   }
 });
